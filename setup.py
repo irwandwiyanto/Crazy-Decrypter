ee1dc91cf633d38131ba60e1675f5293bb83a323
agusmakmun <summon.agus@gmail.com>
2016-05-21 14:14:32 +0700

    Update Version 0.0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name                = "crazy_decrypter",
     version             = "0.0.4",
     url                 = 'https://github.com/agusmakmun/Crazy-Decrypter/',
-    download_url        = 'https://github.com/agusmakmun/Crazy-Decrypter/tarball/v0.0.3',
+    download_url        = 'https://github.com/agusmakmun/Crazy-Decrypter/tarball/v0.0.4',
     description         = 'Python Crazy Decrypter is real crazy tool to decrypt md5, sha1, sha224, sha256, sha384, and sha512 with Brute Force method.',
     license             = 'MIT',
     author              = 'Agus Makmun (Summon Agus)',
